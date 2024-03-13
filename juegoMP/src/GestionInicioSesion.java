8a5ca0a1e8328031c0863ef72b019d74028bc468
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2024-03-13 10:46:52 +0100

    Update GestionInicioSesion.java

--- juegoMP/src/GestionInicioSesion.java
+++ juegoMP/src/GestionInicioSesion.java
@@ -1,4 +1,8 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -7,18 +11,90 @@ import java.util.*;
 public class GestionInicioSesion {
 
     public void menuOptions() {
+        int opcion = 0;
+        while (opcion != 3){
+            System.out.println("Eliga la opción");
+            System.out.println("1.-Registrarse");
+            System.out.println("2.-Iniciar sesión");
+            System.out.println("3.-Salir");
+            opcion = this.leerInt();
+            if (opcion == 1){
+                this.registrarse();
+            } else if (opcion == 2){
+                this.inicioSesion();
+            } else{
+                System.out.println("Hasta la proxima");
+            }
+        }
+    }
+
+    public void inicioSesion() {
         // TODO implement here
     }
 
-    public void InicioSesion() {
-        // TODO implement here
+    public void registrarse() {
+        boolean ok = false;
+        // Crear un objeto File con la ruta de la carpeta
+        File directorio = new File("datos/partida");
+        if (directorio.exists() && directorio.isDirectory()) {//si la carpeta existe
+            // Obtener la lista de archivos en el directorio
+            File[] archivos = directorio.listFiles();
+            Partida p = new Partida();
+            if (archivos != null){ //si existen archivos en la carpeta
+                p = p.deserializar();
+            }//Si esta vacia la carpeta no deserializamos y dejamos la partida a null
+            System.out.println("Introduzca su Nick");
+            String nick = this.leerString();
+            if (this.coincide(nick)){
+                Operador op1 = new Operador();
+                op1.preguntarDetallesOperador();
+                if (op1.getContraseña() != null){
+                    p.setUsuarioActivo(op1);
+                    ok = true;
+                }
+            } else{//falta comprobar si el nick no esta ya en algun jugador
+                Jugador j1 = new Jugador();
+                j1.setNick(nick);
+                j1.preguntarDetallesJugador(p);
+                if (j1.getContraseña() != null) {
+                    p.setUsuarioActivo(j1);
+                    p.getMapJugadores().put(nick, j1);
+                    ok = true;
+                }
+            }
+            if (ok) {
+                p.Play();
+            }
+        }
     }
 
-    public void Registrarse() {
-        // TODO implement here
+    private boolean coincide(String nike){
+        // Crear un objeto File con la ruta de la carpeta
+        File directorio = new File("datos/operadores");
+        if (directorio.exists() && directorio.isDirectory()) {
+            // Obtener el primer archivo del directorio
+            // Obtener la lista de archivos en el directorio
+            File[] archivos = directorio.listFiles();
+
+            // Leer el contenido del primer archivo de texto (si hay alguno)
+            if (archivos != null && archivos.length > 0) {
+                File primerArchivo = archivos[0];
+                if (primerArchivo.isFile() && primerArchivo.getName().endsWith(".txt")) {
+                    try (BufferedReader br = new BufferedReader(new FileReader(primerArchivo))) {
+                        String primeraLinea = br.readLine();
+                        if (primeraLinea != null && primeraLinea.equals(nike)) {
+                            return true;
+                        }
+                    } catch (IOException e) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
-    public void Salir() {
+    public void salir() {
         // TODO implement here
     }
 
@@ -27,9 +103,18 @@ public class GestionInicioSesion {
         return null;
     }
 
-    public Usuario preguntarDetallesR() {
-        // TODO implement here
-        return null;
+    private int leerInt(){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.close();
+        return num;
+    }
+
+    private String leerString(){
+        Scanner scanner = new Scanner(System.in);
+        String valor = scanner.nextLine();
+        scanner.close();
+        return valor;
     }
 
 }
