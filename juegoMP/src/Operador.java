4d5dd2a702089234c4b544a3c2e591d35e32d246
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2024-03-13 10:47:59 +0100

    Update Operador.java

--- juegoMP/src/Operador.java
+++ juegoMP/src/Operador.java
@@ -7,6 +7,29 @@ import java.util.*;
  */
 public class Operador extends Usuario implements Serializable {
 
+    public void preguntarDetallesOperador() {
+        boolean ok = false;
+        int num = 0;
+        while (!ok || num>2){
+            System.out.println("Introduzca nombre");
+            String nombre = this.leerString();
+            this.setNombre(nombre);
+            String contraseña;
+            do {
+                System.out.println("Introduzca contraseña");
+                contraseña = this.leerString();
+            } while (contraseña.length()>12 || contraseña.length()<8);
+            this.setContraseña(contraseña);
+            System.out.println("Introduzca 1 si esta de acuerdo con los datos introducidos");
+            System.out.println("Nombre: " + this.getNombre());
+            System.out.println("Contraseña: " + this.getContraseña());
+            ok = this.leerString().equals("1");
+            num++;
+        }
+        if (num>2){
+            this.setContraseña(null);
+        }
+    }
     @Override
     public void Menu(Partida p) {
         // TODO implement here
@@ -26,5 +49,19 @@ public class Operador extends Usuario implements Serializable {
 
     public void editarPersonaje(Partida p) {
         // TODO implement here
+    }
+
+    private int leerInt(){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.close();
+        return num;
+    }
+
+    private String leerString(){
+        Scanner scanner = new Scanner(System.in);
+        String valor = scanner.nextLine();
+        scanner.close();
+        return valor;
     }
 }
