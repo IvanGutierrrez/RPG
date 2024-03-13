bd8aa7697cbaf1b0567f69e8bb75260772b6bb08
arturox2500 <gutierrezarturox@gmail.com>
2024-03-13 20:10:53 +0100

    test

--- juegoMP/src/JuegoMP.java
+++ juegoMP/src/JuegoMP.java
@@ -4,6 +4,7 @@
 public class JuegoMP {
 
     public static void main() {
+        System.out.println("lmaaao");
         GestionInicioSesion gestion = new GestionInicioSesion();
         gestion.menuOptions();
     }
