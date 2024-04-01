01d751160c39aa15c8ea3e2ff2fa6e3157737713
jae9104 <j.andres.2022@alumnos.urjc.es>
2024-04-01 23:17:07 +0200

    añadidas correciones

--- Trabajo-MP/src/Jugador.java
+++ Trabajo-MP/src/Jugador.java
@@ -195,7 +195,7 @@ public class Jugador extends Usuario implements Serializable {
                     } else if (opcion == 9) {
                         p.darDeBajaUsuario(this);
                     }
-                } else if (this.getDesafio() != null && this.getDesafio().isValido() && this.getDesafio().getGanador() == null && this.getDesafio().getRondas().isEmpty()) {
+                } else if (this.getDesafio() != null && this.getDesafio().isValido() && this.getDesafio().getGanador() == null && (this.getDesafio().getRondas() == null || this.getDesafio().getRondas().isEmpty())) {
                     this.desafiadoResuelve();
                     this.setDesafio(null);
                 } else if (this.getDesafio() != null && this.getDesafio().isValido()) {
